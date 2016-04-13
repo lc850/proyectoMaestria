3a60a0f564913e08919efcb7d8c34d686f982ee2
Eduardo Millan <eduardo_32sharinga@hotmail.com>
2016-04-13 09:01:25 -0600

    menu principal

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Principal</title>
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+</head>
+<body>
+	<div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>UAS</h1>
+                <hr>
+            </div>
+            <div class="col-xs-12">
+				<ul class="nav nav-pills">
+			    	<li class="active" role="presentation"><a>Principal</a></li>
+			    	<li  role="presentation"><a href="">Materias</a></li>
+			    	<li  role="presentation"><a href="">Maestros</a></li>
+			    	<li  role="presentation"><a href="">Alumnos</a></li>
+				</ul>
+			</div>
+			<div class="col-xs-12">
+				<img src="{{ asset('imagenes/uas.png') }}" alt="" width="90px" height="110px" class="img-rounded">
+			</div>
+		</div>
+	</div>
+</body>
+</html>
