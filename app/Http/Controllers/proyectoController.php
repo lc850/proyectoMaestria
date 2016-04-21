fd7d601f8ea7e1d23fea35ab487e8eb70418bedb
Emmanuel Lopez <emmanuelop_17@hotmail.com>
2016-04-21 08:19:23 -0600

    Funcion Maestros

--- app/Http/Controllers/proyectoController.php
+++ app/Http/Controllers/proyectoController.php
@@ -18,8 +18,21 @@ class proyectoController extends Controller
 		return view('principal');
 	}
 
-	public function Maestros($id){
-		$maestros=Maestros::where('id_maestro', $id);
-        return view('maestros', compact('maestros'));
+	public function Maestros(Request $datos){
+		$nombre=$datos->input('nombre');
+        $edad=$datos->input('edad');
+        $sexo=$datos->input('sexo');
+        $materia=$datos->input('materia');
+
+        $nuevo= new Maestros();
+        $nuevo->nombre=$nombre;
+        $nuevo->edad=$edad;
+        $nuevo->sexo=$sexo;
+        $nuevo->materia=$materia;
+        $nuevo->save();
+
+        $maestro=Maestros::all();
+
+        return view('maestros', compact('maestro'));
 	}
 }
